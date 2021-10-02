72b076da92391c553b605145e659e577bd8b4d14
unknown <sauvole75@gmail.com>
2021-10-02 13:39:05 +0700

    add

---
+++ src/main/java/com/example/mvcspringboot/MvcSPringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSPringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcSPringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcspringboot/MvcSPringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSPringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
